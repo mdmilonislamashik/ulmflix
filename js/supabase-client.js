1bb0c4dae3bd7728a9f8a7b45af97287cd47cd31
Milon Islam <mdmilonislamashik@gmail.com>
2026-08-07 19:04:41 +0600

    Fix Supabase client

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,23 +1,23 @@
 ﻿(function () {
 "use strict";
 
-if (!window.supabase || !window.ULMFLIX_SUPABASE) {
-console.error("Supabase config missing");
-return;
+if (!window.supabase || !window.ULMFLIX_CONFIG) {
+    console.error("Supabase configuration missing.");
+    return;
 }
 
 window.SF_SUPABASE = window.supabase.createClient(
-window.ULMFLIX_SUPABASE.URL,
-window.ULMFLIX_SUPABASE.KEY,
-{
-auth: {
-flowType: "pkce",
-persistSession: true,
-autoRefreshToken: true,
-detectSessionInUrl: false
-}
-}
+    window.ULMFLIX_CONFIG.SUPABASE_URL,
+    window.ULMFLIX_CONFIG.SUPABASE_PUBLISHABLE_KEY,
+    {
+        auth: {
+            flowType: "pkce",
+            persistSession: true,
+            autoRefreshToken: true,
+            detectSessionInUrl: false
+        }
+    }
 );
 
-console.log("ULMFlix Supabase connected successfully.");
+console.log("ULMFlix Supabase connected.");
 })();
